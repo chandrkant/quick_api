1550d57588786d19362511a00b809abe10adc535
Chandrakant Solanki <chandrkant.solanki@stellingtech.com>
2018-03-21 10:30:24 +0530

    chnage in config

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
 		json_response(@projects)
 	end
 	def edit
-
+		# sms_message = "Surprise! Dear #{user_name}, we know you love bus trips, RailYatri has upgraded you to the VIP zone, you now get Rs. 300 off on bus tickets! Use code: VIP http://m.rytr.in/home-page"
 	end
 	def show
 
@@ -14,7 +14,9 @@ class ProjectsController < ApplicationController
 
 	end
 	def create
-		puts project_params
+		@project = Project.new(project_params)
+		@project.save
+		json_response(@project) 
 	end
 	def destroy
 
@@ -22,8 +24,8 @@ class ProjectsController < ApplicationController
 	private
 
 	def project_params
-		ActiveModelSerializers::Deserialization.jsonapi_parse!(params, only: [:title, :created_by,:status] )
-		# params.permit(:title, :created_by,:status)
+		# ActiveModelSerializers::Deserialization.jsonapi_parse!(params, only: [:title, :created_by,:status] )
+		params.require(:project).permit(:title, :created_by,:status)
 	end
 
 	def set_project
--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,1 +1,6 @@
-ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+# ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+# # Disable for all serializers (except ArraySerializer)
+ActiveModelSerializers.config.root = true
+
+# Disable for ArraySerializer
+# ActiveModel::ArraySerializer.root = true

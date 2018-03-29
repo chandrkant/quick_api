f5ffa7088543189d01efc462a3e4fe742d95c94d
Chandrakant Solanki <chandrkant.solanki@stellingtech.com>
2018-03-29 17:28:10 +0530

    add controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
 	before_action :set_project, only: [:show, :update, :destroy]
 	def index
-		@projects = Project.all
+		# @projects = Project.all
+		@projects = current_user.projects
 		json_response(@projects)
 	end
 	def edit
-		
+
 	end
 	def show
 		json_response(@project)
@@ -16,8 +17,9 @@ class ProjectsController < ApplicationController
 	end
 	def create
 		@project = Project.new(project_params)
+		@project.user = current_user
 		@project.save
-		json_response(@project) 
+		json_response(@project)
 	end
 	def destroy
    @project.destroy
